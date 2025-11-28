791ecc9e8d0289de6b059431fb7b4da8a5c35847
groupienoseen <ramanenka.andrei@student.ehu.lt>
2025-11-28 18:35:55 +0100

    Implement Student Management System: split code into modules, implement all methods, add file operations and logger with CLI support

---
+++ pr-1/task/utils.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const { getLogger } = require('./Logger');
+const { Student } = require('./Student');
+
+
+function saveToJSON(data, filePath) {
+  const logger = getLogger();
+  try {
+    const jsonData = JSON.stringify(data, null, 2);
+    
+    fs.writeFileSync(filePath, jsonData, "utf8");
+    logger.log("JSON file saved successfully!");
+  } catch (err) {
+    logger.log("Error writing file:", err);
+    throw err;
+  }
+}
+
+function loadJSON(filePath) {
+  const logger = getLogger();
+  if (fs.existsSync(filePath)) {
+    const content = fs.readFileSync(filePath, 'utf8');
+    const jsonData = JSON.parse(content);
+    
+    if (Array.isArray(jsonData)) {
+      return jsonData.map(studentData => 
+        new Student(studentData.id, studentData.name, studentData.age, studentData.group)
+      );
+    }
+    
+    return jsonData;
+  } else {
+    logger.log("File doesn't exist");
+    return null;
+  }
+}
+
+
+module.exports = {loadJSON, saveToJSON}
